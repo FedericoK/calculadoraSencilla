e8440d52c3435616ce603b2712bedbe2999306c0
Federico Kallsen <federicokallsen@gmail.com>
2024-01-09 16:22:43 -0300

    Se parte el string en 3 partes se verifica si son 3 partes Se verificar los operandos si son numeros Se realiza la operacion necesaria dependiendo del operador

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	//fmt para imprimir en consola y formatear a string
 	"fmt"
 	//para convertir strings a numeros
-
+	"strconv"
 	//Manipulacion de strings
 	"strings"
 )
@@ -25,6 +25,45 @@ func main() {
 		input, _ := reader.ReadString('\n')
 		input = strings.TrimSpace(input)
 
-		fmt.Println(strings.TrimSpace(input))
+		//parte en secciones el input del usuario dependiendo de los espacios, en este caso en 3 partes "numero[0] operador[1] numero[2]"
+		partes := strings.Fields(input)
+
+		//Verifica que se haya escrito correctamente la operacion
+		if len(partes) != 3 {
+			//si es incorrecto - mensaje de error y vuelve al incio
+			fmt.Println("Formato incorrecto. Asegurece de usar el formato 'a + b'")
+			continue
+		}
+
+		//definimos los operandos y el operador y convertimos las partes de los operandos [0] y [2] de Sting a Float64
+		operando1, err1 := strconv.ParseFloat(partes[0], 64)
+		operando2, err2 := strconv.ParseFloat(partes[2], 64)
+		operador := partes[1]
+
+		//Verifica la conversion de los operandos cuando se convierten letras a float se hacen 0
+		if err1 != nil || err2 != nil {
+			//si hay error - muestra mensaje y vuelve al inicio
+			fmt.Println("Error: Asegurese de que ambos operandos sean numeros.")
+			continue
+		}
+
+		//Usamos un switch para realizar la operacion dependiendo del mismo
+		switch operador {
+		case "+":
+			fmt.Printf("Resultado: %v\n", operando1+operando2)
+		case "-":
+			fmt.Printf("Resultado: %v\n", operando1-operando2)
+		case "*":
+			fmt.Printf("Resultado: %v\n", operando1*operando2)
+		case "/":
+			//si es division hay que verificar que no divida entre 0
+			if operando2 == 0 {
+				fmt.Println("Error: Division por cero.")
+			} else {
+				fmt.Printf("Resultado: %v\n", operando1/operando2)
+			}
+		default:
+			fmt.Printf("Operador desconocido. Utilice +, -, *, /.")
+		}
 	}
 }
